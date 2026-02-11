1bb58bf0ff03670fcf8cd1887f2eb2cbc1177820
Alexars <ramirezsalinasalexa@gmail.com>
2026-02-10 18:02:12 -0700

    Alexa's commit

--- tech_lab_on_campus/market_watch/producer_and_consumer/producer/producer_interface.py
+++ tech_lab_on_campus/market_watch/producer_and_consumer/producer/producer_interface.py
@@ -11,29 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import pika
+import os
 
 class mqProducerInterface:
     def __init__(self, routing_key: str, exchange_name: str) -> None:
         # Save parameters to class variables
+        self.routing_key = routing_key
+        self.exchange_name = exchange_name
 
-        # Call setupRMQConnection
+        # Call setupRMQConnection'
+        self.setupRMQConnection()
         pass
 
     def setupRMQConnection(self) -> None:
         # Set-up Connection to RabbitMQ service
-
+        conParams = pika.URLParameters(os.environ['AMQP_URL'])
+        self.m_connection = pika.BlockingConnection(parameters=conParams)
         # Establish Channel
 
         # Create the exchange if not already present
-
+        self.m_channel.exchange_declare(self.exchange_name)
         pass
 
     def publishOrder(self, message: str) -> None:
         # Basic Publish to Exchange
+        self.m_channel.basic_publish(self.exchange_name, self.routing_key, body=message)
 
         # Close Channel
+        self.m_channel.close()
 
         # Close Connection
-    
+        self.m_connection.close()
         pass
